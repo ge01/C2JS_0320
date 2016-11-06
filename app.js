f9d5d3a873e8325fa0cfdf16a4cbbd08330b582d
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-11-05 23:00:45 -0500

    variables

---
+++ app.js
@@ -0,0 +1,11 @@
+var month1 = "January",
+    month2 = "February",
+    month3 = "March";
+
+var days1 = 31,
+    days2 = 28,
+    days3 = 33;
+
+var high1 = 22.6,
+    high2 = 37.4,
+    high3 = 53.9;
